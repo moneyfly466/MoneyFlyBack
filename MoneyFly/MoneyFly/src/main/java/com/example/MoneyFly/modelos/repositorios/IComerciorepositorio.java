f218c67f8afd13f20bbb93f8427e117d1c554ca8
Juan Esteban <juanestebanvanegasarango@gmail.com>
2026-04-25 14:57:32 -0500

    HU9 Servicio - HU4 Comercio

--- MoneyFly/MoneyFly/src/main/java/com/example/MoneyFly/modelos/repositorios/IComerciorepositorio.java
+++ MoneyFly/MoneyFly/src/main/java/com/example/MoneyFly/modelos/repositorios/IComerciorepositorio.java
@@ -11,21 +11,6 @@ import com.example.MoneyFly.modelos.Comercio;
 @Repository
 public interface IComerciorepositorio extends JpaRepository<Comercio, Integer> {
 
-    // Consultas personalizadas basadas en los atributos del modelo Comercio
-
-    // Buscar por nombre
-    List<Comercio> findByNombre(String nombre);
-
-    // Buscar por actividad
-    List<Comercio> findByActividad(String actividad);
-
-    // Buscar por sector comercial
-    List<Comercio> findBySectorComercial(String sectorComercial);
-
-    // Buscar por NIT
-    Optional<Comercio> findByNit(String nit);
-
-    // Buscar por ubicación
-    List<Comercio> findByUbicacion(String ubicacion);
+    // Consultas personalizadas 
 
 }
